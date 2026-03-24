b0203180a7cdcfb2a84406029380789200b4ec2d
brent <brentthierens2006@gmail.com>
2026-03-24 22:19:50 +0100

    V0.0.2 - css file linked

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,14 +19,21 @@
             </style>
         @endif
     </head> --}}
+
+
 <head>
-    <title>De sociale kaart</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $title ?? 'De Sociale Kaart' }}</title>
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css','resources/js/app.js'])
+    @endif
+    <link href="{{ asset('files/css/main.css') }}" rel="stylesheet">
 </head>
+
 <body>
+
+
 
 </body>
 </html>
+
